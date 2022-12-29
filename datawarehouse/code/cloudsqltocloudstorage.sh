6c2e141ec6e7c463910d39514c9e73d60745340c
Jesse Mensah <jessemensah21@gmail.com>
2022-12-29 15:35:38 +0000

    eight change

--- datawarehouse/code/cloudsqltocloudstorage.sh
+++ datawarehouse/code/cloudsqltocloudstorage.sh
@@ -6,7 +6,8 @@ gs://$cloudstoragebucketname/mysql_export/stations20180101/stations.csv \
 --offload \ 
 --query='SELECT * FROM stations WHERE station_id <= 200;' 
 
-gcloud sql export mysql-instance-source \ 
+gcloud sql export csv mysql-instance-source \ 
+gs://$cloudstoragebucketname/mysql_export/stations20180102/stations.csv \
 --database=apps_db \ 
 --offload \ 
 -- query='SELECT * FROM stations WHERE station_id <= 400;'
