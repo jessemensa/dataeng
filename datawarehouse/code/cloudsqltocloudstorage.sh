d099da6ce656f264e10b81e20ae58164a52efde3
Jesse Mensah <jessemensah21@gmail.com>
2022-12-29 15:50:35 +0000

    ninth change

--- datawarehouse/code/cloudsqltocloudstorage.sh
+++ datawarehouse/code/cloudsqltocloudstorage.sh
@@ -1,13 +1,13 @@
 cloudstoragebucketname = engexamspreparation-bucket
 
 gcloud sql export csv mysql-instance-source \ 
-gs://$cloudstoragebucketname/mysql_export/stations/20180101/stations.csv \ 
+gs://engexamspreparation-bucket/mysql_export/stations/20180101/stations.csv \ 
 --database=apps_db \ 
 --offload \ 
 --query='SELECT * FROM stations WHERE station_id <= 200;' 
 
 gcloud sql export csv mysql-instance-source \ 
-gs://$cloudstoragebucketname/mysql_export/stations/20180102/stations.csv \
+gs://engexamspreparation-bucket/mysql_export/stations/20180102/stations.csv \
 --database=apps_db \ 
 --offload \ 
 -- query='SELECT * FROM stations WHERE station_id <= 400;'
