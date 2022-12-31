b4d9f2a85512e26f563f1b0ffe3b945240276a4c
Jesse Mensah <jessemensah21@gmail.com>
2022-12-31 16:30:34 +0000

    tweentyfive change

--- datawarehouse/code/loadfacttablledailytrips.py
+++ datawarehouse/code/loadfacttablledailytrips.py
@@ -5,35 +5,40 @@ PROJECT_ID = "engexamspreparation"
 TARGET_TABLE_ID = "{}.dwh_bikesharing.fact_daily_trips".format(PROJECT_ID) 
 
 def createfacttable(PROJECT_ID, TARGET_TABLE_ID):
-    load_date = sys.argv[1] # date format :yyyy-mm-dd 
-    print("\nLoad date:", load_date) 
+    load_date = sys.argv[1] # date format: yyyy-mm-dd
+    print("\nLoad date:", load_date)
 
-    client = bigquery.Client() 
+    client = bigquery.Client()
+
     job_config = bigquery.QueryJobConfig(
-        destination = TARGET_TABLE_ID, 
-        write_disposition = 'WRITE_APPEND'
+        destination=TARGET_TABLE_ID,
+        write_disposition='WRITE_APPEND'
     )
 
     sql = """
-    'SELECT DATE(start_date) as trip_date',
-          'start_station_id',
-          'COUNT(trip_id) as total_trips',
-          'SUM(duration_sec) as sum_duration_sec,
-          'AVG(duration_sec) as avg_duration_sec''
-    'FROM `{PROJECT_ID}.raw_bikesharing.trips` trips'
-    'JOIN `{load_date}.raw_bikesharing.stations` stations'
-    'ON trips.start_station_id = stations.station_id'
-    'WhERE DATE(start_date) = DATE('{}')'
-    'GROUP BY trip_date, start_station_id'
-    ;""".format(PROJECT_ID=PROJECT_ID, load_date=load_date)
+    SELECT DATE(start_date) AS trip_date,
+           start_station_id,
+           COUNT(trip_id) AS total_trips,
+           SUM(duration_sec) AS sum_duration_sec,
+           AVG(duration_sec) AS avg_duration_sec
+    FROM `{PROJECT_ID}.raw_bikesharing.trips` trips
+    JOIN `{PROJECT_ID}.raw_bikesharing.stations` stations
+    ON trips.start_station_id = stations.station_id
+    WHERE DATE(start_date) = DATE(%s)
+    GROUP BY trip_date, start_station_id;
+    """.format(PROJECT_ID=PROJECT_ID)
 
-    query_job = client.query(sql, job_config=job_config) 
+    query_job = client.query(
+        sql,
+        job_config=job_config,
+        query_parameters=(bigquery.ScalarQueryParameter("load_date", "DATE", load_date),)
+    )
 
-    try: 
-        query_job.result() 
-        print("Query success") 
+    try:
+        query_job.result()
+        print("Query success")
     except Exception as exception:
-        print(exception) 
+        print(exception)
 
 if __name__ == "__main__":
     createfacttable(PROJECT_ID, TARGET_TABLE_ID)
