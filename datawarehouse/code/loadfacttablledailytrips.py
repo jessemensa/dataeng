cf0fb3b5623796f8bbae438c005a9f4873228315
Jesse Mensah <jessemensah21@gmail.com>
2022-12-31 16:47:59 +0000

    thirtyone change

--- datawarehouse/code/loadfacttablledailytrips.py
+++ datawarehouse/code/loadfacttablledailytrips.py
@@ -18,12 +18,12 @@ def createfacttable(PROJECT_ID, TARGET_TABLE_ID):
           COUNT(trip_id) as total_trips,
           SUM(duration_sec) as sum_duration_sec,
           AVG(duration_sec) as avg_duration_sec
-          FROM `{PROJECT_ID}.raw_bikesharing.trips` trips
-          JOIN `{load_date}.raw_bikesharing.stations` stations
+          FROM `{}.raw_bikesharing.trips` trips
+          JOIN `{}.raw_bikesharing.stations` stations
           ON trips.start_station_id = stations.station_id
           WHERE DATE(start_date) = DATE(%S)
           GROUP BY trip_date, start_station_id
-          ;""".format(PROJECT_ID=PROJECT_ID)
+          ;""".format(PROJECT_ID, load_date)
 
     query_job = client.query(sql, job_config=job_config)
 
